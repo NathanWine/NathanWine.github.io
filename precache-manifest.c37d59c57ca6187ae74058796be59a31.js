4c65153cc3ada20aaaaa997c6afe55e22b121967
Nathan <illseeyouafterclass@gmail.com>
2019-10-20 20:35:13 -0400

    Updates

---
+++ precache-manifest.c37d59c57ca6187ae74058796be59a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629e2ee3d57cb6d293474daedddcdc0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f649f626935d7e9c69ef",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7107f3d643d52011ce82",
+    "url": "/static/js/2.a56a0bb3.chunk.js"
+  },
+  {
+    "revision": "f649f626935d7e9c69ef",
+    "url": "/static/js/main.8f52ddb2.chunk.js"
+  },
+  {
+    "revision": "7010982bd1f1dbe6f8b0",
+    "url": "/static/js/runtime-main.2a9e45cb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
